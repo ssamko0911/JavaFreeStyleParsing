458aad631d8c62a34127b97d60a7ac65e83a86dc
ssamko0911 <ssamko091184@gmail.com>
2026-02-07 20:00:53 +0000

    Updated labels

--- src/main/java/enums/BookRecordLabel.java
+++ src/main/java/enums/BookRecordLabel.java
@@ -4,6 +4,7 @@ public enum BookRecordLabel {
     OBJECT_SEPARATOR("---------"),
     NEW_RECORD_LOG("New book record has been added: "),
     NEW_AUTHOR_LOG("New Author object has been added:\n%s\n"),
+    PHYSICAL_DESCRIPTION_PARSED("Physical Description record parsed:\n%s\n"),
     TOTAL_RECORDS_LOG("Total records has been added: %d"),
     TO_STRING_SHORT_TEST("LibraryBookRecord:\n1) OCLC Number: %s;\n2) ISBN: %s;\n"),
     TO_STRING("""
