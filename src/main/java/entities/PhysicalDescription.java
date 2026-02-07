4fd7ee4de829ec9d04eda10d21d6364afcbd65b6
ssamko0911 <ssamko091184@gmail.com>
2026-02-07 19:08:12 +0000

    Added PhysicalDescription

---
+++ src/main/java/entities/PhysicalDescription.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.util.Objects;
+
+public class PhysicalDescription {
+    private String pagesInfo;
+    private String dimensions;
+
+    public PhysicalDescription(String pagesInfo, String dimensions) {
+        this.pagesInfo = pagesInfo;
+        this.dimensions = dimensions;
+    }
+
+    public String getPagesInfo() {
+        return this.pagesInfo;
+    }
+
+    public PhysicalDescription setPagesInfo(String pagesInfo) {
+        this.pagesInfo = pagesInfo;
+        return this;
+    }
+
+    public String getDimensions() {
+        return this.dimensions;
+    }
+
+    public PhysicalDescription setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PhysicalDescription that = (PhysicalDescription) o;
+
+        return Objects.equals(pagesInfo, that.pagesInfo) && Objects.equals(dimensions, that.dimensions);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pagesInfo, dimensions);
+    }
+
+    @Override
+    public String toString() {
+        if (this.dimensions.isEmpty()) {
+            return pagesInfo;
+        }
+
+        return this.pagesInfo + " ; " + this.dimensions;
+    }
+}
