e47731e3377ba413d317521fcaf32df1be41f078
ssamko0911 <ssamko091184@gmail.com>
2026-02-07 20:06:47 +0000

    Added SearchCriteria

---
+++ src/main/java/entities/SearchCriteria.java
@@ -0,0 +1,27 @@
+package entities;
+
+import enums.SearchField;
+
+public class SearchCriteria {
+    private final SearchField field;
+    private final String query;
+    private final boolean caseSensitive;
+
+    public SearchCriteria(SearchField field, String query, boolean caseSensitive) {
+        this.field = field;
+        this.query = query;
+        this.caseSensitive = caseSensitive;
+    }
+
+    public SearchField getField() {
+        return field;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+}
