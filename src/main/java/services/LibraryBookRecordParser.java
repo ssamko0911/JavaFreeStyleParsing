9b98adbc3ba8af732cdd17b2da2ec4784b73c55d
ssamko0911 <ssamko091184@gmail.com>
2026-02-08 19:24:19 +0000

    Reformatted LibraryBookRecordParser

--- src/main/java/services/LibraryBookRecordParser.java
+++ src/main/java/services/LibraryBookRecordParser.java
@@ -3,6 +3,8 @@ package services;
 import entities.LibraryBookRecord;
 import enums.ErrorLabel;
 
+import java.util.Set;
+
 public class LibraryBookRecordParser {
     private final static String OCLC = "OCLC Number:";
     private final static String TITLE = "Title:";
@@ -14,6 +16,20 @@ public class LibraryBookRecordParser {
     private final static String PHYSICAL_DESCRIPTION = "Physical Description:";
     private final static String GENRE = "Genre:";
     private final static String ISBN = "ISBN:";
+
+    private static final Set<String> FIELD_LABELS = Set.of(
+            LibraryBookRecordParser.OCLC,
+            LibraryBookRecordParser.TITLE,
+            LibraryBookRecordParser.AUTHOR_PLURAL,
+            LibraryBookRecordParser.AUTHOR_SINGULAR,
+            LibraryBookRecordParser.SUMMARY,
+            LibraryBookRecordParser.PUBLICATION_YEAR,
+            LibraryBookRecordParser.PUBLISHER,
+            LibraryBookRecordParser.PHYSICAL_DESCRIPTION,
+            LibraryBookRecordParser.GENRE,
+            LibraryBookRecordParser.ISBN
+    );
+
     private final AuthorParser authorParser;
     private final PhysicalDescriptionParser physicalDescriptionParser;
     private final PublisherParser publisherParser;
@@ -68,17 +84,6 @@ public class LibraryBookRecordParser {
     }
 
     public boolean isFieldLabel(String line) {
-        String trimmedLine = line.trim();
-
-        return trimmedLine.equals(LibraryBookRecordParser.OCLC) ||
-                trimmedLine.equals(LibraryBookRecordParser.TITLE) ||
-                trimmedLine.equals(LibraryBookRecordParser.AUTHOR_PLURAL) ||
-                trimmedLine.equals(LibraryBookRecordParser.AUTHOR_SINGULAR) ||
-                trimmedLine.equals(LibraryBookRecordParser.SUMMARY) ||
-                trimmedLine.equals(LibraryBookRecordParser.PUBLICATION_YEAR) ||
-                trimmedLine.equals(LibraryBookRecordParser.PUBLISHER) ||
-                trimmedLine.equals(LibraryBookRecordParser.PHYSICAL_DESCRIPTION) ||
-                trimmedLine.equals(LibraryBookRecordParser.GENRE) ||
-                trimmedLine.equals(LibraryBookRecordParser.ISBN);
+        return FIELD_LABELS.contains(line.trim());
     }
 }
