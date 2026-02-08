5aeee2dc06d91e636186d21316151ba513dc6708
ssamko0911 <ssamko091184@gmail.com>
2026-02-08 19:40:09 +0000

    Added PublicationYearParser

---
+++ src/main/java/services/PublicationYearParser.java
@@ -0,0 +1,17 @@
+package services;
+
+import enums.ErrorLabel;
+
+public class PublicationYearParser {
+    private final static String PUBLICATION_YEAR = "Year of publication:";
+
+    public int parsePublicationYear(String publicationYearLine) {
+        try {
+            return Integer.parseInt(publicationYearLine);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(
+                    String.format(ErrorLabel.PUBLICATION_YEAR_INVALID_VALUE.getLabel(), PublicationYearParser.PUBLICATION_YEAR, publicationYearLine)
+            );
+        }
+    }
+}
