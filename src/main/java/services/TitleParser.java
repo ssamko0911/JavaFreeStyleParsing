004c94a165ce5c9e16643de36ec49bdc0a3d84a0
ssamko0911 <ssamko091184@gmail.com>
2026-02-08 20:13:18 +0000

    Added TitleParser

---
+++ src/main/java/services/TitleParser.java
@@ -0,0 +1,29 @@
+package services;
+
+import entities.Title;
+import util.AppLogger;
+
+public class TitleParser {
+    private final static String PATTERN = ":";
+    private static final AppLogger logger = AppLogger.getInstance();
+
+    public Title parseTitle(String titleLine) {
+        if (titleLine == null || titleLine.isEmpty()) {
+            TitleParser.logger.warn("TitleLine is null or empty: " + titleLine);
+            return new Title(null, null);
+        }
+
+        String[] titleParts = titleLine.split(TitleParser.PATTERN);
+
+        Title title;
+        if (titleParts.length == 1) {
+            title = new Title(titleParts[0], null);
+        } else {
+            title = new Title(titleParts[0], titleParts[1]);
+        }
+
+        TitleParser.logger.info("Title parsed: " + title);
+
+        return title;
+    }
+}
