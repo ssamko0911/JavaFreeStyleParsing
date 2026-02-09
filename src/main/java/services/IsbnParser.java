0c4392d913ae55849abb79849d8bdfd5e0307a62
ssamko0911 <ssamko091184@gmail.com>
2026-02-09 13:16:32 +0000

    Added ISBN checksum validation

--- src/main/java/services/IsbnParser.java
+++ src/main/java/services/IsbnParser.java
@@ -19,11 +19,11 @@ public class IsbnParser {
         String[] isbnArray = isbnLine.split(PATTERN);
 
         IsbnNumber isbnNumber;
-        if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_13_LENGTH) {
+        if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_13_LENGTH && this.isValidIsbn13(isbnArray[0])) {
             isbnNumber = new IsbnNumber(isbnArray[0], null);
-        } else if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_10_LENGTH) {
+        } else if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_10_LENGTH && this.isValidIsbn10(isbnArray[0])) {
             isbnNumber = new IsbnNumber(null, isbnArray[0]);
-        } else if (isbnArray.length == 2 && this.validateIsbnLength(isbnArray)) {
+        } else if (isbnArray.length == 2 && this.validateIsbnLength(isbnArray) && this.isValidIsbn13(isbnArray[0]) && this.isValidIsbn10(isbnArray[1])) {
             isbnNumber = new IsbnNumber(isbnArray[0], isbnArray[1]);
         } else {
             IsbnParser.logger.warn("Failed to parse isbn: " + isbnLine);
@@ -39,5 +39,25 @@ public class IsbnParser {
     private boolean validateIsbnLength(String[] isbnArray) {
         return isbnArray[0].length() == IsbnParser.ISBN_13_LENGTH &&
                 isbnArray[1].length() == IsbnParser.ISBN_10_LENGTH;
+    }
+
+    private boolean isValidIsbn13(String isbn13) {
+        int sum = 0;
+        for (int i = 0; i < IsbnParser.ISBN_13_LENGTH; i++) {
+            int digit = isbn13.charAt(i) - '0';
+            sum += (i % 2 == 0) ? digit : digit * 3;
+        }
+
+        return sum % 10 == 0;
+    }
+
+    private boolean isValidIsbn10(String isbn10) {
+        int sum = 0;
+        for (int i = 0; i < IsbnParser.ISBN_10_LENGTH; i++) {
+            int value = (i == 9 && isbn10.charAt(i) == 'X') ? 10 : isbn10.charAt(i) - '0';
+            sum += value * (i + 1);
+        }
+
+        return sum % 11 == 0;
     }
 }
