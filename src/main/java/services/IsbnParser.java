aee3d3e8f499d26f11da7d73c6fca15cb2e46a55
ssamko0911 <ssamko091184@gmail.com>
2026-02-09 11:05:03 +0000

    Refactored IsbnParser

--- src/main/java/services/IsbnParser.java
+++ src/main/java/services/IsbnParser.java
@@ -5,10 +5,12 @@ import util.AppLogger;
 
 public class IsbnParser {
     private final static String PATTERN = ", ";
+    private final static int ISBN_13_LENGTH = 13;
+    private final static int ISBN_10_LENGTH = 10;
     private static final AppLogger logger = AppLogger.getInstance();
 
     public IsbnNumber parseIsbn(String isbnLine) {
-        if (isbnLine == null || isbnLine.isEmpty()) {
+        if (isbnLine != null || !isbnLine.isEmpty()) {
             IsbnParser.logger.warn("Failed to parse isbn: " + isbnLine);
 
             return new IsbnNumber(null, null);
@@ -17,15 +19,25 @@ public class IsbnParser {
         String[] isbnArray = isbnLine.split(PATTERN);
 
         IsbnNumber isbnNumber;
-        if (isbnArray.length == 1) {
+        if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_13_LENGTH) {
             isbnNumber = new IsbnNumber(isbnArray[0], null);
-        } else {
+        } else if (isbnArray.length == 1 && isbnArray[0].length() == IsbnParser.ISBN_10_LENGTH) {
+            isbnNumber = new IsbnNumber(null, isbnArray[0]);
+        } else if (isbnArray.length == 2 && this.validateIsbnLength(isbnArray)) {
             isbnNumber = new IsbnNumber(isbnArray[0], isbnArray[1]);
-        }
+        } else {
+            IsbnParser.logger.warn("Failed to parse isbn: " + isbnLine);
 
+            return new IsbnNumber(null, null);
+        }
 
         IsbnParser.logger.info("ISBN parsed: " + isbnNumber);
 
         return isbnNumber;
+    }
+
+    private boolean validateIsbnLength(String[] isbnArray) {
+        return isbnArray[0].length() == IsbnParser.ISBN_13_LENGTH &&
+                isbnArray[1].length() == IsbnParser.ISBN_10_LENGTH;
     }
 }
