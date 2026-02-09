8d66cfa2109cd3d1dcaae061b2ced65d9efb0f62
ssamko0911 <ssamko091184@gmail.com>
2026-02-09 10:39:15 +0000

    Added IsbnNumber

---
+++ src/main/java/entities/IsbnNumber.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class IsbnNumber {
+    public final String isbn13;
+    public final String isbn10;
+
+
+    public IsbnNumber(String isbn13, String isbn10) {
+        this.isbn13 = isbn13;
+        this.isbn10 = isbn10;
+    }
+
+    public String getIsbn13() {
+        return isbn13;
+    }
+
+    public String getIsbn10() {
+        return isbn10;
+    }
+
+    @Override
+    public String toString() {
+        if (this.isbn10 == null || this.isbn10.isEmpty()) {
+            return this.isbn13;
+        }
+
+        return String.format("%s, %s", this.isbn13, this.isbn10);
+    }
+}
