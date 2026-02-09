6ed6302d830fbd5b86f5b28f991201349e7a37df
ssamko0911 <ssamko091184@gmail.com>
2026-02-09 21:42:30 +0000

    Updated labels

--- src/main/java/enums/FrameLabel.java
+++ src/main/java/enums/FrameLabel.java
@@ -8,7 +8,15 @@ public enum FrameLabel {
     APP_TITLE("LibraryBookService App"),
     READY("Ready"),
     LOADING("Loading "),
-    LOADED("Loaded: %s (%d bytes) | %d words | %d chars | %d book%s");
+    LOADED("Loaded: %s (%d bytes) | %d words | %d chars | %d book%s"),
+    TABLE_COL_TITLE("Title"),
+    TABLE_COL_AUTHOR("Authors"),
+    TABLE_COL_YEAR("Publication Year"),
+    TABLE_COL_GENRE("Genre"),
+    TAB_DETAILS("Details"),
+    TAB_RAW_TEXT("Raw Text"),
+    NO_SELECTION("None"),
+    SHOW_ALL("Show All");
 
     private final String label;
 
