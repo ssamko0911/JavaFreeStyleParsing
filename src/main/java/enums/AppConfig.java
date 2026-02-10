99957d5e78d08f098b34108313a9d4dea38c3fda
ssamko0911 <ssamko091184@gmail.com>
2026-02-10 23:23:06 +0000

    Updated config

--- src/main/java/enums/AppConfig.java
+++ src/main/java/enums/AppConfig.java
@@ -4,8 +4,6 @@ public enum AppConfig {
     APP_WIDTH("app.width", "950"),
     APP_HEIGHT("app.height", "680"),
     APP_TEXT_AREA_EDITABLE("app.text_area_editable", "false"),
-    APP_TEXT_AREA_ROWS("app.textAreaRows", "30"),
-    APP_TEXT_AREA_COLS("app.textAreaCols", "30"),
     APP_LOG_FILE("app.logFile", "logs/appLog.log"),
     APP_TABLE_ROW("app.tableRowHeight", "24"),
     APP_SPLIT_DIVIDER("app.splitDividerLocation", "450"),
