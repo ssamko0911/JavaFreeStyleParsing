35793a9405e3275b0b292430be691ff940889a1b
ssamko0911 <ssamko091184@gmail.com>
2026-02-10 23:54:07 +0000

    Added colors

--- src/main/java/app/FileChooserApp.java
+++ src/main/java/app/FileChooserApp.java
@@ -46,6 +46,7 @@ public class FileChooserApp extends JFrame {
     private static final AppLogger logger = AppLogger.getInstance();
 
     public FileChooserApp() {
+        this.getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.GREEN);
         this.setTitle(FrameLabel.APP_TITLE.getLabel());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(FileChooserApp.APP_WIDTH, FileChooserApp.APP_HEIGHT);
@@ -93,25 +94,33 @@ public class FileChooserApp extends JFrame {
         this.toolBar.setFloatable(false);
 
         this.chooseFileButton = new JButton(FrameLabel.CHOOSE_FILE.getLabel());
+        this.chooseFileButton.setForeground(Color.decode("#81C784"));
         this.clearTextButton = new JButton(FrameLabel.CLEAN_CONTENT.getLabel());
+        this.clearTextButton.setForeground(Color.decode("#81C784"));
         this.validationReportButton = new JButton(FrameLabel.REPORT.getLabel());
+        this.validationReportButton.setForeground(Color.decode("#81C784"));
         this.searchButton = new JButton(FrameLabel.SEARCH.getLabel());
+        this.searchButton.setForeground(Color.decode("#81C784"));
         this.showAllButton = new JButton(FrameLabel.SHOW_ALL.getLabel());
+        this.showAllButton.setForeground(Color.decode("#81C784"));
 
         this.tableModel = new BookTableModel();
         this.bookTable = new JTable(this.tableModel);
         this.bookTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         this.bookTable.setAutoCreateRowSorter(true);
+        this.bookTable.setForeground(Color.decode("#90EE90"));
 
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(JLabel.CENTER);
         this.bookTable.getColumnModel().getColumn(2).setCellRenderer(centerRenderer);
 
         this.detailTextArea = new JTextArea();
+        this.detailTextArea.setForeground(Color.decode("#90EE90"));
         this.detailTextArea.setEditable(FileChooserApp.TEXT_AREA_EDITABLE);
 
         this.rawTextArea = new JTextArea();
         this.rawTextArea.setEditable(FileChooserApp.TEXT_AREA_EDITABLE);
+        this.rawTextArea.setForeground(Color.decode("#90EE90"));
 
         this.detailTabbedPane = new JTabbedPane();
 
