30fb584122961f7122885660a060090edf2e8788
ssamko0911 <ssamko091184@gmail.com>
2026-02-12 00:56:20 +0000

    Added OclcReportByGenreService (WIP)

---
+++ src/main/java/services/OclcReportByGenreService.java
@@ -0,0 +1,13 @@
+package services;
+
+import javax.swing.table.DefaultTableModel;
+
+public class OclcReportByGenreService {
+    //DefaultTableModel tableModel;
+
+    public void generateReport() {};
+
+    private void generateReportHeader() {};
+
+    private void generateReportRaw() {};
+}
