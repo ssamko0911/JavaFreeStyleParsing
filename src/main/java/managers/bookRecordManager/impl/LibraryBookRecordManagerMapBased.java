ddeb9b81821b4f2d149015de9641eb61359e8ec5
ssamko0911 <ssamko091184@gmail.com>
2026-02-12 00:54:42 +0000

    Added LibraryBookRecordManagerMapBased

---
+++ src/main/java/managers/bookRecordManager/impl/LibraryBookRecordManagerMapBased.java
@@ -0,0 +1,34 @@
+package managers.bookRecordManager.impl;
+
+import entities.libraryItems.LibraryBookRecord;
+import managers.bookRecordManager.LibraryBookRecordManageable;
+
+import java.util.*;
+
+public class LibraryBookRecordManagerMapBased implements LibraryBookRecordManageable {
+    private final Map<String, LibraryBookRecord> bookRecords = new HashMap<>();
+
+    public void addRecord(LibraryBookRecord libraryBookRecord) {
+        bookRecords.put(libraryBookRecord.getOclcNumber(), libraryBookRecord);
+    }
+
+    public Collection<LibraryBookRecord> getBookRecords() {
+        return bookRecords.values();
+    }
+
+    public int getBookRecordsCount() {
+        return bookRecords.size();
+    }
+
+    public void clearRecords() {
+        bookRecords.clear();
+    }
+
+    public boolean supportsLookupByKey() {
+        return true;
+    }
+
+    public LibraryBookRecord findByKey(String key) {
+        return this.bookRecords.get(key);
+    }
+}
