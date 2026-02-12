eb6db0d3dd2fe3e1f1b2a1a459313f4e1dd753c1
ssamko0911 <ssamko091184@gmail.com>
2026-02-12 00:54:12 +0000

    Added LibraryBookRecordManageable

---
+++ src/main/java/managers/bookRecordManager/LibraryBookRecordManageable.java
@@ -0,0 +1,21 @@
+package managers.bookRecordManager;
+
+import entities.libraryItems.LibraryBookRecord;
+
+import java.util.Collection;
+
+public interface LibraryBookRecordManageable {
+    void addRecord(LibraryBookRecord libraryBookRecord);
+
+    Collection<LibraryBookRecord> getBookRecords();
+
+    int getBookRecordsCount();
+
+    void clearRecords();
+
+    boolean supportsLookupByKey();
+
+    default LibraryBookRecord findByKey(String key){
+        return null;
+    }
+}
