fa427812ad613b2ff18e5d94ed0909dd7d27c8f2
ssamko0911 <ssamko091184@gmail.com>
2026-02-19 12:49:04 +0000

    Added LibraryDvdRecordParser

---
+++ src/main/java/services/parsers/LibraryDvdRecordParser.java
@@ -0,0 +1,69 @@
+package services.parsers;
+
+import entities.libraryItems.LibraryDvdRecord;
+import services.LibraryItemParser;
+
+import java.util.Set;
+
+public class LibraryDvdRecordParser implements LibraryItemParser<LibraryDvdRecord> {
+    private final static String OCLC = "OCLC Number:";
+    private final static String TITLE = "Title:";
+    private final static String CAST = "Cast:";
+    private final static String CREDITS = "Credits:";
+    private final static String PLOT = "Plot:";
+    private final static String RELEASE_YEAR = "Year of release:";
+    private final static String LANGUAGE = "Language:";
+    private final static String PUBLISHER = "Publisher:";
+    private final static String GENRE = "Genre:";
+    private final static String PHYSICAL_DESCRIPTION = "Physical Description:";
+    private final static String ISBN = "ISBN:";
+
+    private static final Set<String> FIELD_LABELS = Set.of(
+            LibraryDvdRecordParser.OCLC,
+            LibraryDvdRecordParser.TITLE,
+            LibraryDvdRecordParser.CAST,
+            LibraryDvdRecordParser.CREDITS,
+            LibraryDvdRecordParser.PLOT,
+            LibraryDvdRecordParser.RELEASE_YEAR,
+            LibraryDvdRecordParser.LANGUAGE,
+            LibraryDvdRecordParser.PUBLISHER,
+            LibraryDvdRecordParser.GENRE,
+            LibraryDvdRecordParser.PHYSICAL_DESCRIPTION,
+            LibraryDvdRecordParser.ISBN
+    );
+
+    private final TitleParser titleParser;
+    private final IsbnParser isbnParser;
+
+    public LibraryDvdRecordParser(TitleParser titleParser, IsbnParser isbnParser) {
+        this.titleParser = titleParser;
+        this.isbnParser = isbnParser;
+    }
+
+    @Override
+    public LibraryDvdRecord createRecord() {
+        return new LibraryDvdRecord();
+    }
+
+    @Override
+    public boolean isFieldLabel(String line) {
+        return FIELD_LABELS.contains(line.trim());
+    }
+
+    @Override
+    public void setField(LibraryDvdRecord record, String field, String value) {
+        switch (field.trim()) {
+            case LibraryDvdRecordParser.OCLC -> record.setOclcNumber(value);
+            case LibraryDvdRecordParser.TITLE -> record.setTitle(this.titleParser.parseTitle(value));
+            case LibraryDvdRecordParser.CAST -> record.setCast(value);
+            case LibraryDvdRecordParser.CREDITS -> record.setCredits(value);
+            case LibraryDvdRecordParser.PLOT -> record.setPlot(value);
+            case LibraryDvdRecordParser.RELEASE_YEAR -> record.setReleaseYear(Integer.parseInt(value));
+            case LibraryDvdRecordParser.LANGUAGE -> record.setLanguage(value);
+            case LibraryDvdRecordParser.PUBLISHER -> record.setPublisher(value);
+            case LibraryDvdRecordParser.GENRE -> record.setGenre(value);
+            case LibraryDvdRecordParser.PHYSICAL_DESCRIPTION -> record.setPhysicalDescription(value);
+            case LibraryDvdRecordParser.ISBN -> record.setIsbn(this.isbnParser.parseIsbn(value));
+        }
+    }
+}
